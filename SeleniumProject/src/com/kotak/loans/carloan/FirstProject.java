a0d88002d3c33f690480a04bb4b11e6e954d2bda
mbiswal1 <motilal.biswal@gmail.com>
2019-06-05 10:07:45 +0530

    SelenumProject1
    added SelenumProject1

---
+++ SeleniumProject/src/com/kotak/loans/carloan/FirstProject.java
@@ -0,0 +1,10 @@
+package com.kotak.loans.carloan;
+
+public class FirstProject {
+
+	public static void main(String[] args) {
+		System.out.println("Hi!");
+
+	}
+
+}
---
+++ SeleniumProject/src/com/kotak/loans/carloan/SecondProject.java
@@ -0,0 +1,8 @@
+package com.kotak.loans.carloan;
+
+public class SecondProject {
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+	}
+
+}
